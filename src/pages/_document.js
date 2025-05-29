14518ba13d4b6a9097cca3ce05ea1686701ca83e
Moez Khalid <29011087+mightymozdef@users.noreply.github.com>
2025-05-29 13:39:57 -0400

    fixing build issues

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,30 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          {/* <link rel='stylesheet' href='/path/to/your/custom/styles.css' /> */}
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        {/* <link rel='stylesheet' href='/path/to/your/custom/styles.css' /> */}
 
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='anonymous'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
--- src/pages/experiences/[slug].js
+++ src/pages/experiences/[slug].js
@@ -1,30 +1,32 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import Layout from "../../components/Layout";
-import { experiences as experiencesData } from "../../utils/data";
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Layout from '../../components/Layout'
+import { experiences as experiencesData } from '../../utils/data'
 
 const ExperienceDetail = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  const [experience, setExperience] = useState(null);
+  const router = useRouter()
+  const { slug } = router.query
+  const [experience, setExperience] = useState(null)
 
   useEffect(() => {
     if (slug) {
-      const foundExperience = experiencesData.find((exp) => exp.slug === slug);
-      setExperience(foundExperience);
+      const foundExperience = experiencesData.find((exp) => exp.slug === slug)
+      setExperience(foundExperience)
     }
-  }, [slug]);
+  }, [slug])
 
   if (!experience) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
     <Layout>
-      <h1 className="text-3xl font-bold">{experience.title}</h1>
-      <div className="mt-4">
+      <h1 className='text-3xl font-bold'>{experience.title}</h1>
+      <div className='mt-4'>
         <p>{experience.description}</p>
       </div>
     </Layout>
-  );
+  )
 }
+
+export default ExperienceDetail
